8dbc7ba530ce3d4332e21535743d830bb1269b39
Paulo Vitor Ferreira Goncalves Pivotto <paulo_vitor652@hotmail.com>
2026-08-18 11:58:32 -0300

    Add visit counter using sessions framework

--- catalog/views.py
+++ catalog/views.py
@@ -5,32 +5,38 @@ from .models import Book, Author, BookInstance, Genre
 
 def index(request):
     """View function for home page of site."""
-    
+
     # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
-    
+
     # Available books (status = 'a')
-    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_instances_available = BookInstance.objects.filter(status__exact="a").count()
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
-    
+
     num_genres = Genre.objects.count()
-    
-    num_books_the = Book.objects.filter(title__icontains='the').count()
+
+    num_books_the = Book.objects.filter(title__icontains="the").count()
+
+    # Number of visits to this viwe, as counted in the session variable
+    num_visits = request.session.get("num_visits", 0)
+    num_visits += 1
+    request.session["num_visits"] = num_visits
 
     context = {
-        'num_books': num_books,
-        'num_instances': num_instances,
-        'num_instances_available': num_instances_available,
-        'num_authors': num_authors,
-        'num_genres': num_genres,
-        'num_books_the': num_books_the,
+        "num_books": num_books,
+        "num_instances": num_instances,
+        "num_instances_available": num_instances_available,
+        "num_authors": num_authors,
+        "num_genres": num_genres,
+        "num_books_the": num_books_the,
+        "num_visits": num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
-    return render(request, 'index.html', context=context)
+    return render(request, "index.html", context=context)
 
 
 class BookListView(generic.ListView):
